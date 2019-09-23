dce1a93a8744f91bebdf791e6dc8def23b78cbf6
Kevin Jahns <kevin.jahns@protonmail.com>
2019-09-23 12:52:39 +0200

    linting

--- codemirror/index.js
+++ codemirror/index.js
@@ -1,39 +1,40 @@
 /* eslint-env browser */
 
-import * as Y from "yjs";
-import { WebsocketProvider } from "y-websocket";
-import { CodeMirrorBinding } from "y-codemirror";
-import CodeMirror from "codemirror";
+import * as Y from 'yjs'
+import { WebsocketProvider } from 'y-websocket'
+import { CodeMirrorBinding } from 'y-codemirror'
+import CodeMirror from 'codemirror'
 import 'codemirror/mode/javascript/javascript.js'
 
-window.addEventListener("load", () => {
-  const ydoc = new Y.Doc();
+window.addEventListener('load', () => {
+  const ydoc = new Y.Doc()
   const provider = new WebsocketProvider(
-    `${location.protocol === "http:" ? "ws:" : "wss:"}${location.host}`,
-    "codemirror",
+    `${location.protocol === 'http:' ? 'ws:' : 'wss:'}${location.host}`,
+    'codemirror',
     ydoc
-  );
-  const yText = ydoc.getText("codemirror");
-  const editorContainer = document.createElement("div");
-  editorContainer.setAttribute("id", "editor");
-  document.body.insertBefore(editorContainer, null);
+  )
+  const yText = ydoc.getText('codemirror')
+  const editorContainer = document.createElement('div')
+  editorContainer.setAttribute('id', 'editor')
+  document.body.insertBefore(editorContainer, null)
 
   const editor = CodeMirror(editorContainer, {
-    mode: "javascript"
-  });
+    mode: 'javascript',
+    lineNumbers: true
+  })
 
-  const binding = new CodeMirrorBinding(yText, editor, provider.awareness);
+  const binding = new CodeMirrorBinding(yText, editor, provider.awareness)
 
-  const connectBtn = document.getElementById("y-connect-btn");
-  connectBtn.addEventListener("click", () => {
+  const connectBtn = document.getElementById('y-connect-btn')
+  connectBtn.addEventListener('click', () => {
     if (provider.shouldConnect) {
-      provider.disconnect();
-      connectBtn.textContent = "Connect";
+      provider.disconnect()
+      connectBtn.textContent = 'Connect'
     } else {
-      provider.connect();
-      connectBtn.textContent = "Disconnect";
+      provider.connect()
+      connectBtn.textContent = 'Disconnect'
     }
-  });
+  })
 
-  window.example = { provider, ydoc, yText, binding };
-});
+  window.example = { provider, ydoc, yText, binding }
+})
