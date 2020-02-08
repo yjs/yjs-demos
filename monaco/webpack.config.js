0e5f4fa78ecb9ffe4cdf23181a9c8c9da709d914
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-02-08 23:04:43 +0100

    Fix issues as requested

--- monaco/webpack.config.js
+++ monaco/webpack.config.js
@@ -30,4 +30,3 @@ module.exports = {
     publicPath: '/monaco/dist/'
   }
 }
-  
--- tiptap/src/Realtime.js
+++ tiptap/src/Realtime.js
@@ -1,42 +1,28 @@
-// Copyright (c) 2019 Dirk Holtwick. All rights reserved. https://holtwick.de/copyright
-
 import { keymap } from 'prosemirror-keymap'
 import { Extension } from 'tiptap'
-import { redo, undo, ySyncPlugin, yUndoPlugin } from 'y-prosemirror'
+import { redo, undo, yCursorPlugin, ySyncPlugin, yUndoPlugin } from 'y-prosemirror'
+import { WebsocketProvider } from 'y-websocket'
 import * as Y from 'yjs'
 
 const ydoc = new Y.Doc()
-
-ydoc.on('update', (updateMessage, origin) => {
-  console.log('update', updateMessage, origin)
-})
-
-// const provider = new WebsocketProvider('wss://demos.yjs.dev', 'prosemirror', ydoc)
+const provider = new WebsocketProvider('wss://demos.yjs.dev', 'tiptap', ydoc)
 const type = ydoc.getXmlFragment('prosemirror')
 
 export default class RealtimeExtension extends Extension {
-
-  get name() {
+  get name () {
     return 'realtime'
   }
 
-  // get defaultOptions() {
-  //   return {
-  //     emptyNodeClass: 'is-empty'
-  //   }
-  // }
-
-  get plugins() {
+  get plugins () {
     return [
       ySyncPlugin(type),
-      // yCursorPlugin(provider.awareness),
+      yCursorPlugin(provider.awareness),
       yUndoPlugin(),
       keymap({
         'Mod-z': undo,
         'Mod-y': redo,
-        'Mod-Shift-z': redo,
-      }),
+        'Mod-Shift-z': redo
+      })
     ]
   }
-
 }
