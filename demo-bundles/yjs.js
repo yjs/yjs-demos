b10ab689f4a7ecdd10db5907c823846bceb7b464
Kevin Jahns <kevin.jahns@pm.me>
2020-06-27 21:25:03 +0200

    bundle jsfiddle fix

--- demo-bundles/yjs.js
+++ demo-bundles/yjs.js
@@ -11,3 +11,11 @@ window.Y = Y
 window.WebsocketProvider = WebsocketProvider
 window.WebrtcProvider = WebrtcProvider
 window.IndexeddbPersistence = IndexeddbPersistence
+
+// This is a fix for input events in jsfiddle.
+// When the same jsfiddle is included several times as an iframe,
+// the input event is fired in both windows because it propagates up.
+// There is no need to propagate input events to the parent window.
+document.addEventListener('input', event => {
+  event.stopPropagation()
+})
