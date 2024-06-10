537f37bab92510fde15745b702876e31e2145bec
Kevin Jahns <kevin.jahns@protonmail.com>
2024-06-10 20:40:08 +0200

    add monaco-react demo

---
+++ monaco-react/src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+
+import './global.css'
+
+const root = createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(<App />)
