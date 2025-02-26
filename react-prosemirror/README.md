56f53d9e9997528e7740efa01b700991d5e8fdf2
Kevin Jahns <kevin.jahns@protonmail.com>
2025-02-26 14:44:05 +0100

    added react-prosemirror example

---
+++ react-prosemirror/README.md
@@ -0,0 +1,2 @@
+# react-prosemirror
+> Short example on how to set up `@nytimes/prosemirror` with y-prosemirror
---
+++ react-prosemirror/index.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react"
+import { createRoot } from "react-dom/client"
+import { EditorState } from "prosemirror-state"
+import { ProseMirror } from "@nytimes/react-prosemirror"
+
+import { exampleSetup } from 'prosemirror-example-setup'
+import { schema } from "prosemirror-schema-basic"
+import {keymap} from "prosemirror-keymap"
+import {baseKeymap} from "prosemirror-commands"
+
+import * as Y from 'yjs'
+import { WebsocketProvider } from 'y-websocket'
+import { ySyncPlugin, yCursorPlugin, yUndoPlugin, undo, redo, initProseMirrorDoc } from 'y-prosemirror'
+
+
+const ydoc = new Y.Doc()
+const provider = new WebsocketProvider(
+  'wss://demos.yjs.dev/ws', // use the public ws server
+  // `ws${location.protocol.slice(4)}//${location.host}/ws`, // alternatively: use the local ws server (run `npm start` in root directory)
+  'prosemirror-demo-2024/06',
+  ydoc
+)
+
+function App () {
+  const yXmlFragment = ydoc.getXmlFragment('prosemirror')
+  const { doc, mapping } = initProseMirrorDoc(yXmlFragment, schema)
+  const defaultState = EditorState.create({ 
+    doc,
+    schema,
+    plugins: [
+      ySyncPlugin(yXmlFragment, { mapping }),
+      yCursorPlugin(provider.awareness),
+      yUndoPlugin(),
+      keymap({
+        'Mod-z': undo,
+        'Mod-y': redo,
+        'Mod-Shift-z': redo
+      }),
+      keymap(baseKeymap)
+    ].concat(exampleSetup({ schema }))
+  });
+
+
+  // It's important that mount is stored as state,
+  // rather than a ref, so that the ProseMirror component
+  // is re-rendered when it's set
+  const [mount, setMount] = useState(null);
+
+  return (
+    <ProseMirror mount={mount} defaultState={defaultState}>
+      <div ref={setMount} />
+    </ProseMirror>
+  );
+}
+
+setTimeout(() => {
+  const root = createRoot(document.getElementById('root'))
+  root.render(<App />)
+}, 1000)
